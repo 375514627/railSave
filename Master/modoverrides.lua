ae9335c9517fe42937dcab20535f865990d71c4f
375514627 <375514627@qq.com>
2016-12-22 19:51:24 +0800

    2016/12/22 周四

---
+++ Master/modoverrides.lua
@@ -0,0 +1,175 @@
+return {
+  ["e12-462434129"]={
+    configuration_options={
+      FLOODING_INTERVAL=2,
+      FLOODING_PROTECTION=false,
+      FORCE_DROP_ALL=false,
+      IGNORING_ADMIN=true,
+      RESTART_WELCOME_TIPS=false 
+    },
+    enabled=true 
+  },
+  ["steam-10161215"]={
+    configuration_options={
+      deluxeEndoFirepitBurnRate=0.75,
+      deluxeFirepitBurnRate=0.75,
+      dropLoot="yes",
+      endoDropLoot="yes",
+      heatStarBurnRate=0.9,
+      heatStarDropLoot="yes",
+      iceStarBurnRate=0.9,
+      iceStarDropLoot="yes",
+      recipeCost="standard",
+      starsSpawnHounds="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010002"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010007"]={
+    configuration_options={ fast_eat="On", fast_pick_build_harvest_heal="On" },
+    enabled=true 
+  },
+  ["workshop-100010010"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010011"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010012"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010013"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010014"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010015"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010016"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010017"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010018"]={ configuration_options={ kong=0, maxdiaoluo=4, mindiaoluo=1 }, enabled=true },
+  ["workshop-100010020"]={
+    configuration_options={
+      RUOYINXIAN=false,
+      SHENGDANSHI=false,
+      baoshibaolv=1,
+      blue_baoshi=1,
+      er_shuoming=0,
+      green_baoshi=1,
+      marble_suipian=1,
+      orange_baoshi=1,
+      purple_baoshi=1,
+      red_baoshi=1,
+      shitoubaolv=1,
+      thulecite_xiukuang=1,
+      wajuecishu=2,
+      yellow_baoshi=1,
+      yi_shuoming=0 
+    },
+    enabled=true 
+  },
+  ["workshop-100010022"]={
+    configuration_options={ dont_give_dubloons=2, give_dubloons=1, position=1, rec_dubloon=3, rec_gold=3 },
+    enabled=true 
+  },
+  ["workshop-100010023"]={
+    configuration_options={
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-100010024"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="yees",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-100010026"]={ configuration_options={ Time="default" }, enabled=true },
+  ["workshop-100010031"]={
+    configuration_options={ TIANSHU="99999", XIANTIME=false, XIAN_Language="EN" },
+    enabled=true 
+  },
+  ["workshop-100010033"]={ configuration_options={ speed="0" }, enabled=true },
+  ["workshop-100010034"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010039"]={
+    configuration_options={
+      bossres=true,
+      companion=true,
+      dist=2.5,
+      language="AUTO",
+      ownership=0,
+      rebounddmg=0,
+      recipe="normal" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010041"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
+  ["workshop-100010043"]={
+    configuration_options={
+      campfire=true,
+      coldfire=true,
+      coldfirepit=true,
+      deluxe_firepit=true,
+      endo_firepit=true,
+      fire_rez_health_level="default",
+      fire_rez_hunger_level="default",
+      fire_rez_lighting_strike_on_rez=true,
+      fire_rez_sanity_level="default",
+      firepit=true,
+      heat_star=true,
+      ice_star=true,
+      null_option=true 
+    },
+    enabled=true 
+  },
+  ["workshop-100010052"]={ configuration_options={ speed="1.5" }, enabled=true },
+  ["workshop-100010058"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-100010059"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010062"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-100010063"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-100010067"]={
+    configuration_options={
+      DROPGLASSES=0,
+      buffgo=false,
+      fhl_cos=0.05,
+      fhl_hjopen=false,
+      openli=false,
+      openlight=true,
+      zzj_cankanshu=true,
+      zzj_canuseashammer=false,
+      zzj_canuseasshovel=false,
+      zzj_canwakuang=true,
+      zzj_finiteuses=0,
+      zzj_fireopen=false,
+      zzj_pre=1.5,
+      zzj_times=0.05 
+    },
+    enabled=true 
+  },
+  ["workshop-100010071"]={
+    configuration_options={ BXSZ="0", LLSZ=1, Ming="scripts/0.lua", NSSZ="N" },
+    enabled=true 
+  }
+}
